1c415c2ef54642623f55e5724ceb108fb6fa669b
Arjun BR <84361554+Arjunbr121@users.noreply.github.com>
2022-11-25 18:27:22 +0530

    Add files via upload

---
+++ tic-tac-toe.js
@@ -0,0 +1,67 @@
+console.log("Welcome to tic tac toe")
+let music = new Audio("win.mp3")
+let audioturn = new Audio("click.mp3")
+let gameover = new Audio("loss.mp3")
+let turn = "X"
+let isgameover=false;
+
+//Function to change the turn
+const changeTurn = ()=>{
+    return turn === "X"?"O":"X"
+}
+
+//function to check for win
+const checkwin = ()=>{
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins =[
+        [0, 1, 2, -15, 5, 0],
+        [3, 4, 5, -15, 15, 0],
+        [6, 7, 8, -15, 25, 0],
+        [0, 3, 6, -25.5, 15, 90],
+        [1, 4, 7, -15, 15, 90],
+        [2, 5, 8, -5, 15, 90],
+        [0, 4, 8, -15, 15, 45],
+        [2, 4, 6, -15, 15, 135],
+    ]
+    wins.forEach(e =>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText+ " Won ";
+            isgameover = true;
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+            document.querySelector(".line").style.width = "20vw";
+       }
+    })
+}
+
+//Game logic
+//music.play()
+let boxes=document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>{
+    let boxtext =element.querySelector('.boxtext');
+    element.addEventListener('click',()=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText = turn;
+            turn=changeTurn();
+            audioturn.play();
+            checkwin();
+
+            if (!isgameover){
+                document.getElementsByClassName("info")[0].innerText  = " Turn for " + turn;
+            } 
+        }
+    })
+})
+
+//add onclick rest button
+reset.addEventListener('click', ()=>{
+    let boxtexts =document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element =>{
+        element.innerText = ""
+    });
+    turn="X"
+    isgameover=false
+    document.querySelector('.line').style.width ='0vw';
+    document.getElementsByClassName("info")[0].innerText = " Text for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width ="0px"
+})
